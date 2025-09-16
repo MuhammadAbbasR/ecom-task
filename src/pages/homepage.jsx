530b8d92b0eeb622076866a6b3161620caacc21c
Muhammad Abbas <ar457711@gmail.com>
2025-09-16 23:03:04 +0500

    Describe changes 3

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../stores/slices/cartSlices";
-
+import laptop from "src/assests/laptop.png" 
+import headphone from "src/assests/headphone.jpg" 
+import phone from"src/assests/phone.jpg"
+import tablet from "src/assests/tablet.jpg" 
 
 const HomePage = () => {
   const dispatch = useDispatch();
 
   const products = [
-    { id: 1, name: "Laptop", price: 999, img: "src/assests/laptop.png" },
-    { id: 2, name: "Headphones", price: 199, img: "src/assests/headphone.jpg" },
-    { id: 3, name: "Phone", price: 699, img: "src/assests/phone.jpg" },
-    { id: 4, name: "Tablet", price: 499, img: "src/assests/tablet.jpg" },
+    { id: 1, name: "Laptop", price: 999, img: laptop },
+    { id: 2, name: "Headphones", price: 199, img: headphone},
+    { id: 3, name: "Phone", price: 699, img: phone},
+    { id: 4, name: "Tablet", price: 499, img: tablet },
   ];
 
   return (
